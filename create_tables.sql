269005fc571105c8b3af461edd9dccd1008fcd52
unknown <1percentag3@gmail.com>
2025-07-04 09:42:58 +0530

    commit message

---
+++ create_tables.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS demo;
+USE demo;
+CREATE TABLE students (
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(100),
+ marks INT
+);
+INSERT INTO students (name, marks) VALUES
+(Pranav, 95),
+(Pratham, 80),
+(Omkar, 90);
